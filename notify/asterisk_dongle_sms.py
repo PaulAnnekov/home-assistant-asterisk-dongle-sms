ebd2cd9dabf3079aa38e0720f14b651ed22989fb
Paul Annekov <paul.annekov@gmail.com>
2017-03-11 15:46:06 +0200

    fixed non-existing arguments

--- notify/asterisk_dongle_sms.py
+++ notify/asterisk_dongle_sms.py
@@ -28,7 +28,7 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
 _LOGGER = logging.getLogger(__name__)
 
 
-def get_service(hass, config):
+def get_service(hass, config, discovery_info=None):
     """Get the Asterisk notification service."""
     dongle = config.get(CONF_DONGLE)
     address = config.get(CONF_ADDRESS)
